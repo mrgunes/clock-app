08c3b88babb328279100e2e789a7dceed1555ba8
kaancatik <72238525+kaancatik@users.noreply.github.com>
2021-03-27 16:16:52 -0700

    Added css rules.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div className='app'>
+      <button className='toogle'>Dark Mode</button>
+      <div className='clock-container'>
+        <div className='clock'>
+          <div className='needle hour'></div>
+          <div className='needle minute'></div>
+          <div className='needle second'></div>
+          <div className='center-point'></div>
+        </div>
+        <div className='time'>12:00</div>
+        <div className='date'>Sunday, March <span className='circle'>27</span></div>
+      </div>
     </div>
   );
 }
