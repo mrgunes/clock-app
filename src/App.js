1ea5d274b45ecb00a8bd64d288f808973161161c
kaancatik <72238525+kaancatik@users.noreply.github.com>
2021-03-27 16:55:50 -0700

    Added dark mode-light mode functionality.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  let days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  let months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+  let [toggle, setToggle]=useState('Dark Mode');
+
+  let toogleHandle=(e)=>{
+    let html=document.querySelector('html')
+    if(html.classList.contains('dark')){
+      html.classList.remove('dark');
+      setToggle('Dark Mode');
+     }else{
+       html.classList.add('dark')
+       setToggle('Light Mode')
+     }
+  }
+
   return (
     <div className='app'>
-      <button className='toogle'>Dark Mode</button>
+      <button className='toogle' onClick={toogleHandle}>{toggle}</button>
       <div className='clock-container'>
         <div className='clock'>
           <div className='needle hour'></div>
