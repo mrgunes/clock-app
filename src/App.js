9a07a1dcc4505f877a4fb4f7cb37dd8bea9e7cd8
kaancatik <72238525+kaancatik@users.noreply.github.com>
2021-03-28 00:38:52 -0700

    Added circle for clock and fixed 00:00 format.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   //let [second, setSecond]=useState(0);
   let time= new Date();
-  // console.log(time);
+  //console.log(time);
 
   let scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -31,23 +31,32 @@ function App() {
      }
   }
 
-  // let minutes=time.getMinutes();
-  // let seconds=time.getMinutes();
-  // let AMPMHours=hours % 12;
-  // let month= time.getMonth();
-  // let day=time.getDay();
-  // let date=time.getDate()
-
   useEffect(()=>{
     let timer=setInterval(()=>{
       setState({
-        hour: scale(time.getHours()%12, 0, 12, 0, 360),
+        hour: scale(time.getHours()%12, 0, 11, 0, 360),
         minute: scale(time.getMinutes(), 0, 59, 0, 360),
         second: scale(time.getSeconds(), 0, 59, 0, 360)})
     }, 1000);
     return () => clearInterval(timer);
   },[state]);
   console.log(state)
+
+  let smallHour=()=>{
+    if (time.getHours()<10){
+      return `0${time.getHours()}`
+    } else{
+      return time.getHours()
+    }
+  };
+
+  let smallMinutes=()=>{
+    if (time.getMinutes()<10){
+      return `0${time.getMinutes()}`
+    } else {
+      return time.getMinutes()
+    }
+  }
 
   return (
     <div className='app'>
@@ -59,7 +68,7 @@ function App() {
           <div className='needle second' style={{transform: `translate(-50%, -100%) rotate(${state.second}deg)`}}></div>
           <div className='center-point'></div>
         </div>
-        <div className='time'>{`${time.getHours()}:${time.getMinutes() < 10 ? `0${time.getMinutes}` : `${time.getMinutes()}`}`}</div>
+        <div className='time'>{`${smallHour()}:${smallMinutes()}`}</div>
         <div className='date'>{`${days[time.getDay()]}`}, {`${months[time.getMonth()]}`} <span className='circle'>{`${time.getDate()}`}</span></div>
       </div>
     </div>
